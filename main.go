a40f9e3f49914ff62a9cfba45d6db09b2d89a30c
Shahzod9941 <shahzodtoatov@gmail.com>
2020-01-05 08:50:39 +0300

    test for nps added

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//salaries := [...]int{10_000, 12_000, 15_00}
+	//max := salaries[0]
+	//for _, salary := range salaries  {
+	//	if
+	fmt.Println("hello")
+
+	}
+}
